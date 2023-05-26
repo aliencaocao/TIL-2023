12db7c32523c139ce82852a9474e32fefc078eab
cheongalc <22002891+cheongalc@users.noreply.github.com>
2023-05-26 18:24:56 +0800

    modified script to convert OD pred into reidformat

--- CV/utils/extract-predicted-bboxes.py
+++ CV/utils/extract-predicted-bboxes.py
@@ -2,16 +2,19 @@ import csv
 import os
 import shutil
 from pathlib import Path
-
+import pandas as pd
+import json
 from PIL import Image
 from tqdm import tqdm
 
-preds_csv_path = Path("../RT-DETR/submissions/0.88x conf 0.8 no reid lb 0.305.csv/")
+preds_csv_path = Path("../RT-DETR/submissions/0.880 noise aug epoch22 conf 0.8 no reid lb 0.305.csv")
 test_imgs_path = Path("../RT-DETR/dataset/test/images/")
 output_path = Path("../RT-DETR/dataset/reid/test/")
 suspect_imgs_path = Path("../RT-DETR/dataset/reid/suspects/")
 
 os.makedirs(output_path, exist_ok=True)
+
+mapping_dict = {}
 
 with open(preds_csv_path) as preds_file:
   reader = csv.reader(preds_file)
@@ -43,5 +46,17 @@ with open(preds_csv_path) as preds_file:
     os.makedirs(gallery_dir, exist_ok=True)
     os.makedirs(query_dir, exist_ok=True)
 
-    img_cropped.save(gallery_dir / f"-1_c{cam_id}s1_1_{num_dets[cam_id]}.png")
-    shutil.copy(suspect_imgs_path / (filename + ".png"), query_dir)
+    img_save_path = gallery_dir / f"-1_c{cam_id}s1_1_{num_dets[cam_id]}.png"
+    # img_cropped.save(img_save_path)
+    # shutil.copy(suspect_imgs_path / (filename + ".png"), query_dir)
+
+    mapping_dict[str(img_save_path)] = {
+      'confidence': row[2],
+      'ymin': y1,
+      'xmin': x1,
+      'ymax': y2,
+      'xmax': x2
+    }
+
+with open('test_set_bbox_mapping_odnoise.json', 'w') as f:
+  json.dump(mapping_dict, f)
