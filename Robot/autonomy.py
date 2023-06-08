Add fuzzy text search for ASR digits Fix unit conversion in dynamic deviation threshold Update TIL SDK and robot code to fix bug in SDK and new You Still Have Checkpoints response

Robot/autonomy.py
@@ -194,8 +194,10 @@ def main():
                     path = [curr_loi, curr_loi] # 2 copies for legacy reasons... prob works with just 1 copy too but that would need testing
                     REACHED_THRESHOLD_LAST_M *= 2/3               
                     continue # Resume movement
+                elif info == 'You Still Have Checkpoints':
+                    logger.warning('Robot has reached end goal without finishing all task points!')  # TODO: what to do sia
                 else:
-                    raise Exception("DSTA rep_service.check_pose() error: Unexpected string value.")
+                    raise ValueError(f"DSTA rep_service.check_pose() error: Unexpected string value: {info}.")
             elif isinstance(info, RealPose):  # Robot reached detour checkpoint and received new coordinates to go to.
                 logger.info(f"Reached detour point, got next task point: {info}.")
                 is_task_not_detour = False
@@ -245,6 +247,8 @@ def main():
                     logger.info(f'Correct password! Moving to next task checkpoint at {target_pose}')
                 elif target_pose_check == 'End Goal Reached':
                     logger.info(f'Correct password! Moving to final goal at {target_pose}')
+                elif target_pose_check == 'You Still Have Checkpoints':
+                    logger.warning('Correct password and target pose given is end goal, however you have missed some previous task checkpoints!')  # TODO: what to do sia
                 else:
                     logger.error(f'Pose gotten from report digit is invalid! Got {target_pose}, check result {target_pose_check}.')
 
@@ -259,7 +263,7 @@ def main():
             logger.info('Planning path to: {}'.format(curr_loi))
             path = planner.plan(pose[:2], curr_loi, display=True)
             if path is None:
-                logger.info('Error, no possible path found to the next location!')
+                logger.error('No possible path found to the next location!')
                 # It should never come to this!
                 # TODO: Implement some simple random movement for the robot to change its location
 
@@ -363,7 +367,7 @@ def main():
                 # MAX_DEVIATION_THRESH_M based on shortest distance to a wall from the straight line that connects to the next wp
                 # This is only calculated at the first iter of every new wp and kept constant when approaching the same wp, to prevent angle threshold increasing too much when the robot is near the wp
                 if prev_wp != curr_wp:
-                    MAX_DEVIATION_THRESH_M = planner.min_clearance_along_path_real(pose, curr_wp)
+                    MAX_DEVIATION_THRESH_M = planner.min_clearance_along_path_real(pose, curr_wp) / 100  # returns cm so convert to m here
                     ANGLE_THRESHOLD_DEG = np.clip(np.degrees(np.arctan2(MAX_DEVIATION_THRESH_M, dist_to_wp)), 5, 25)  # TODO: tune the clamp range
                     NavLogger.debug(f'MAX_DEVIATION_THRESH_M: {MAX_DEVIATION_THRESH_M}, ANGLE_THRESHOLD_DEG: {ANGLE_THRESHOLD_DEG}')
                     prev_wp = curr_wp

Robot/nlp_service.py
@@ -11,10 +11,12 @@ import logging
 
 from TensorRT_Inference import TRTInference
 
+import Levenshtein
 import torchaudio
 from tilsdk.localization.types import *
 from transformers import Wav2Vec2Processor
 import language_tool_python
+import numpy as np
 
 logger = logging.getLogger('NLPService')
 
@@ -76,14 +78,50 @@ class ASRService:
                 output = self.model({'input': audio})['output'][0]  # remove batch dimension
                 outputs.append(output)
             outputs = (self.clean(anno) for anno in self.processor.batch_decode(np.argmax(outputs, axis=-1)))
-            outputs = tuple(self.fix_grammar(anno) for anno in outputs)
-            logger.debug(f'Predicted texts: {outputs}')
-            outputs = (self.find_digit(anno) for anno in outputs)
-            outputs = tuple(o for o in outputs if o is not None)
-            logger.info(f'Predicted digits: {outputs}')
-            if len(outputs) != len(audio_paths):
-                logger.warning(f'{len(audio_paths) - len(outputs)} has no predicted digits!')  # TODO: add fuzzy retrival in this case, perhaps using Levenshtein Distance
-            return outputs
+            outputs_text = tuple(self.fix_grammar(anno) for anno in outputs)
+            logger.debug(f'Predicted texts: {outputs_text}')
+            outputs = tuple(self.find_digit(anno) for anno in outputs_text)  # contains None
+            assert len(outputs) == len(audio_paths)  # make sure every audio has a corresponding output, even if it has no predicted digits
+            outputs_without_None = tuple(o for o in outputs if o is not None)
+            logger.info(f'Predicted digits: {outputs_without_None}')
+            if len(outputs_without_None) != len(audio_paths):
+                # Do fuzzy retrieval of closest word that matches any of the digits using Levenshtein Distance and common word heuristics
+                logger.warning(f'{len(audio_paths) - len(outputs_without_None)} audio(s) has no predicted digits! Using Levenshtein Distance to do fuzzy retrieval')
+                outputs = np.array(outputs)  # convert to np array for faster index finding ops
+                # noinspection PyComparisonWithNone
+                None_idx = np.where(outputs == None)[0]
+                missing_texts = [outputs_text[i] for i in None_idx]
+                outputs_text = list(outputs_text)  # to allow item assignment below
+                for idx, s in zip(None_idx, missing_texts):
+                    s = s.split()
+                    digits_dist = []
+                    for digit in self.digits:
+                        digits_dist.append([Levenshtein.distance(digit, word) for word in s])
+                    digits_dist = np.array(digits_dist)
+                    min_dist = np.min(np.min(digits_dist, axis=0), axis=0)
+                    min_dist_idx = np.where(digits_dist == min_dist)
+                    if len(min_dist_idx) == 1:  # there is only 1 word that has the lowest distance to any of the digits, just take it
+                        logger.info(f'Replacing {s[min_dist_idx[0][0]]} with {self.digits[min_dist_idx[0][1]]}')
+                        s[min_dist_idx[0][0]] = self.digits[min_dist_idx[0][1]]
+                        outputs_text[idx] = ' '.join(s)
+                        logger.info(f'Predicted text after fuzzy retrieval: {outputs_text[idx]}')
+                    else:
+                        logger.warning('Multiple words have the same distance to the digits. Choosing 1 based on heuristics')
+                        common_words = ['TO', 'THE', 'THEY', 'HE', 'SHE', 'A', 'WE']  # if these words are the one that is closest to the digit, then we choose the next one as these are common words and unlikely to be the digit. E.g. TWO -> TO has distance of only 1 but it is most likely wrong
+                        for i in range(len(min_dist_idx)):
+                            if s[min_dist_idx[i][0]] in common_words:
+                                continue
+                            else:
+                                logger.info(f'Replacing {s[min_dist_idx[i][0]]} with {self.digits[min_dist_idx[i][1]]}')
+                                s[min_dist_idx[0][0]] = self.digits[min_dist_idx[0][1]]
+                                outputs_text[idx] = ' '.join(s)
+                                logger.info(f'Predicted text after fuzzy retrieval: {outputs_text[idx]}')
+                                break
+                outputs = (self.find_digit(anno) for anno in outputs_text)  # should not contain None anymore
+                outputs_without_None = tuple(o for o in outputs if o is not None)
+                assert len(outputs_without_None) == len(audio_paths)  # make sure every audio has a corresponding predicted digit
+                logger.info(f'Predicted digits after fuzzy retrieval: {outputs_without_None}')
+            return outputs_without_None
         except Exception as e:
             logger.error(f'Error while predicting: {e}')
             return None